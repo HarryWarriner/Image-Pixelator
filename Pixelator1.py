3b1a96cb1fb733bdda4c36c1cbb89d5d9ac5c8be
HarryWarriner <harry@warriner.org>
2025-08-07 16:46:15 +0100

    Comment

--- Pixelator1.py
+++ Pixelator1.py
@@ -1,13 +1,10 @@
 import cv2
 
 def pixelate_image(image_path, pixel_size=10):
-    # Read the image
     image = cv2.imread(image_path)
-
-    # Get image dimensions
     height, width = image.shape[:2]
 
-    # Resize image to small size
+    # Resize image
     small_image = cv2.resize(image, (width // pixel_size, height // pixel_size), interpolation=cv2.INTER_LINEAR)
 
     # Resize back to original size
@@ -15,11 +12,11 @@ def pixelate_image(image_path, pixel_size=10):
 
     return pixelated_image
 
-# Load and pixelate an image
+# Load and pixelate
 image_path = "Cookie1.jpg"
 pixelated = pixelate_image(image_path, pixel_size=5)
 
-# Show and save the result
+# save
 cv2.imshow("Pixelated Image", pixelated)
 cv2.imwrite("pixelated_output.jpg", pixelated)
 cv2.waitKey(0)
